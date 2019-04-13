ccacf3cd4a98f1c1dcee481f1b29a550a907c05e
Fiona <40315836@live.napier.ac.uk>
2019-04-13 21:39:09 +0100

    SQL Reports

--- src/main/java/com/napier/sem/CountryPop.java
+++ src/main/java/com/napier/sem/CountryPop.java
@@ -3,34 +3,14 @@ package com.napier.sem;
 public class CountryPop
 {
     /**
-     * Country code
-     */
-    public String country_code;
-
-    /**
      * Country name
      */
     public String country_name;
 
     /**
-     * Continent
-     */
-    public String continent;
-
-    /**
-     * Region
-     */
-    public String region;
-
-    /**
      * Country population
      */
     public int population;
-
-    /**
-     * Capital City Name
-     */
-    public String capitalName;
 
     /**
      *  City populations
